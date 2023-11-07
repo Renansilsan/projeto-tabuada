a2d6c703da164cbe3f1af8af2964e597db1c47f8
Renansilsan <141248258+Renansilsan@users.noreply.github.com>
2023-11-07 14:07:48 -0300

    Upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function tabuada() {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+
+    if (num.value.leigth == 0) {
+        window.alert('por favor digite um número')
+    }
+
+    else {
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+        while (c <= 100) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
